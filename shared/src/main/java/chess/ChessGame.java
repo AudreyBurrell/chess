e2aed14c0c3dfe1da0f7598c7948597025684723
AudreyBurrell <audreyb2165@gmail.com>
2026-01-31 13:31:55 -0700

    Adding in the test cases for phase 1

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -46,7 +46,8 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+//        throw new RuntimeException("Not implemented");
+
     }
 
     /**
