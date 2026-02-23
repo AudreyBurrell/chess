b72dd6f401a828cef44b4c8a233e1e221e93ebed
AudreyBurrell <audreyb2165@gmail.com>
2026-02-23 15:11:20 -0700

    Adding in the clear and register endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,59 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
 import io.javalin.*;
+import model.UserData;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
 
 public class Server {
 
     private final Javalin javalin;
+    private final UserService userService;
+    private final GameService gameService;
+    private final ClearService clearService;
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        var dataAccess = new MemoryDataAccess();
+        userService = new UserService(dataAccess);
+        gameService = new GameService(dataAccess);
+        clearService = new ClearService(dataAccess);
 
         // Register your endpoints and exception handlers here.
+        javalin.delete("/db", this::clear);
+        javalin.post("/user", this::register);
 
+
+    }
+    private void clear(io.javalin.http.Context ctx) {
+        try {
+            clearService.clearService();
+            ctx.status(200);
+            ctx.json("{}");
+        } catch (DataAccessException error) {
+            ctx.status(500);
+            ctx.json("{\"message\": \"Error: " + error.getMessage() + "\"}");
+        }
+    }
+    private void register(io.javalin.http.Context ctx) {
+        try {
+            var user = new Gson().fromJson(ctx.body(), UserData.class);
+            if(user.username() == null || user.password() == null || user.email() == null){
+                ctx.status(400);
+                ctx.json("{\"message\": \"Error: bad request\"}");
+                return;
+            }
+            var auth = userService.register(user);
+            ctx.status(200);
+            ctx.json(new Gson().toJson(auth));
+        } catch (DataAccessException error) {
+            ctx.status(500);
+            ctx.json("{\"message\": \"Error: " + error.getMessage() + "\"}");
+        }
     }
 
     public int run(int desiredPort) {
