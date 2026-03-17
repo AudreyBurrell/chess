c79ac92de5d4d2431be8a0e1f8e44fe5c9a6f8f4
AudreyBurrell <audreyb2165@gmail.com>
2026-03-17 11:52:40 -0600

    Adding code based off of PetShop (editing it so it works) and the video

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,77 @@
+package client;
+
+import java.net.http.HttpClient;
+import com.google.gson.Gson;
+import model.*;
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String url) { serverUrl = url; }
+
+//    public RegisterResult register(RegisterRequest request) {
+//        //http work goes here
+//        //http request to call the web api, get back result
+//        //handles all the serialization and deserialization
+//    }
+//    public LoginResult login(LoginRequest request) {...}
+//
+//    public JoinResult joinGame(JoinRequest request) {...}
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws Exception {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new Exception(ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws Exception {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw new Exception(body);
+            }
+
+            throw new Exception("other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
+
+
+}
